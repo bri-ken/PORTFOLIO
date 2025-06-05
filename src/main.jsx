3419db187357e214d2cbad75395b88d8c721c7dd
k6nbri <kengbrian17@gmail.com>
2025-06-05 15:57:50 +0800

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { config } from '@fortawesome/fontawesome-svg-core';
+
+// Import Font Awesome core
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 
-// Prevent Font Awesome from adding its CSS since we did it manually above
-config.autoAddCss = false;
+// Add all icons to the library
+library.add(fas);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
